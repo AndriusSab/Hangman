de1e2347693cc8c4950389a92466ea0f82202b81
Andrius Sabaliauskas <andriussa@print-it.lt>
2023-08-08 19:43:41 +0300

    adding updated cli version

--- hangman_game.py
+++ hangman_game.py
@@ -11,7 +11,7 @@ class HangmanGame:
         self.words_list: List[str] = get_random_words_list()
         self.secret_word: str = random.choice(self.words_list).lower()
         self.guesses: List[str] = []
-        self.max_attempts: int = 7
+        self.max_attempts: int = 10
         self.hangman_states: List[str] = hangman_states
         self.current_hangman_state: int = 0
 
@@ -36,15 +36,19 @@ class HangmanGame:
         print(self.hangman_states[self.current_hangman_state])
 
 
-    def make_guess(self, letter: str) -> bool:
-        letter = letter.lower()
-        if letter in self.guesses:
+    def make_guess(self, guess: str) -> bool:
+        guess = guess.lower()
+        if guess == self.secret_word:
+            self.guesses.append(guess)
+            return True
+        elif guess in self.guesses:
             return False
-        self.guesses.append(letter)
-        if letter not in self.secret_word:
-            self.max_attempts -= 1
-            self.current_hangman_state += 1
-        return True
+        else:
+            self.guesses.append(guess)
+            if guess not in self.secret_word:
+                self.max_attempts -= 1
+                self.current_hangman_state += 1
+            return False
 
     def is_game_over(self) -> bool:
         if self.max_attempts <= 0 or self.current_hangman_state >= len(self.hangman_states):
@@ -60,41 +64,39 @@ class HangmanGame:
             print("\n Attempts left:", self.max_attempts)
             print("\n Word:", self.display_word())
             self.display_hangman()
-            guess = input("Guess a letter: ")
+            guess = input("Guess a letter or word: ").lower()
 
             if not guess.isalpha():
-                print("Please enter a letter.")
+                print("Please enter letters only.")
                 continue
 
-            guess = guess.lower()
-
-            if len(guess) != 1:
-                print("Please enter a single letter.")
-                continue
-
-            if guess in self.guesses:
-                print("You've already guessed that letter.")
-                continue
-            
-            if guess in self.secret_word:
-                self.make_guess(guess)
-                print("\n Correct guess! Keep it up!")
-
-
-            if guess not in self.secret_word:
-                self.make_guess(guess)
-                print("\n Incorrect guess! Try again")
-                
-            else:
-                if self.is_game_over():
+            if len(guess) == 1:
+                if guess in self.guesses:
+                    print("You've already guessed that letter.")
+                    continue
+                if guess in self.secret_word:
+                    self.make_guess(guess)
+                    print("\n Correct guess! Keep it up!")
+                else:
+                    self.make_guess(guess)
+                    print("\n Incorrect guess! Try again")
+            elif len(guess) > 1:
+                if guess == self.secret_word:
+                    self.make_guess(guess)
+                    print("\n Congratulations! You've guessed the word:", self.secret_word)
                     break
+                else:
+                    self.make_guess(guess)
+                    print("\n Incorrect guess! Try again")
 
+            if self.is_game_over():
+                break
         if "_" not in self.display_word():
-            print("Congratulations! You've guessed the word:", self.secret_word)
+            print("\n Congratulations! You've guessed the word:", self.secret_word)
         else:
-            print("Sorry, you're out of attempts. The word was:", self.secret_word)
+            print("\n Sorry, you're out of attempts. The word was:", self.secret_word)
             
-        play_again = input("Do you want to play again? (Y/N): ")
+        play_again = input("\n Do you want to play again? (Y/N): ")
         
         if play_again.upper() == "Y":
             self.__init__()  
